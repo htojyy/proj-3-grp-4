d6ef74a81072142e427db4d9891f720a981ad4e2
htojyy <wyk27@duck.com>
2024-04-17 22:34:31 +1000

    clean up repo, update app.py, add html to templates

--- app.py
+++ app.py
@@ -4,28 +4,17 @@
 
 # Import the dependencies
 # https://hevodata.com/learn/python-mongodb-connection/
-from flask import Flask, render_template, request, url_for, redirect, jsonify
-import pymongo
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 # https://www.digitalocean.com/community/tutorials/how-to-use-mongodb-in-a-flask-application
 from pymongo import MongoClient
-import numpy as np
-import datetime as dt
-from dateutil.relativedelta import relativedelta
-from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# import seaborn.objects as so
 from io import BytesIO
 import base64
-import plotly.graph_objects as go
 import matplotlib
 matplotlib.use('agg')
-
-# https://kumudithaudaya.medium.com/display-a-table-in-a-database-in-html-using-prettytable-python-70d7325f2e46
-# Import from_db_cursor from prettytable library
-from prettytable import from_db_cursor
 
 #################################################
 # Database Setup
@@ -42,17 +31,18 @@ mongo = PyMongo(app)
 
 # Set up MongoDB connection 
 # https://www.geeksforgeeks.org/sending-data-from-a-flask-app-to-mongodb-database/
-client = MongoClient('mongodb://localhost:27017/') 
-db = client['influencers']
+client = MongoClient('mongodb://localhost:27017/')
 
+
+db = client['influencers']
+collection = db.all_platforms
+df = pd.DataFrame(list(collection.find()))
 
 #################################################
 # Flask Setup
 #################################################
 # Create an app
 app = Flask(__name__)
-
-# Define routes
 
 # Home page - list all routes
 @app.route("/")
@@ -64,101 +54,95 @@ def homepage():
         f"View the top 100 influencers by social media platform:<br/>"
         f"/platforms<br/><br/>"
 
-        f"Valid routes to view influencers by top #x, valid numbers between 1 - 100 <br/>" # across different platforms or all? make a table?
-        f"E.g. /10<br/><br/>"
-        f"Valid routes to view top influencers ranked between (1 - 100):<br/>"
-        f"/1/100<br/><br/>" # {start_number}/{end_number}
-        f"View the list of all countries where top 100 influences are from:<br/>"
+        f"Routes to view influencers by top #x, valid numbers between 1 - 100 <br/>"
+        f"E.g. /Top10<br/><br/>"
+
+        f"Find your favourite influencer in this list:<br/>"
+        f"/influencersList<br/><br/>"
+
+        f"See which platforms your favourite influencer is on:<br/>"
+        f"/influencers<br/><br/>"
+
+        f"Bar plot showing average potential reach by social media platform and category<br/>"
+        f"/PotentialReach_categories<br/><br/>"
+
+        f"Box plot showing distribution of potential reach by social media platform<br/>"
+        f"/PotentialReach_box<br/><br/>"
+
+        f"Distinct list of all countries where top 100 influencers are from:<br/>"
         f"/countries<br/><br/>"
-        f"Valid routes to view influencers from the list of countries in /countries:<br/>"
-        f"'input country name here'<br/>"
-        f"E.g. /United%20States<br/>"
+
+        f"Maps of Top 100 influencers across the world:<br/>"
+        f"/map_followers<br/>"
+        f"/map_engagement<br/>"
     )
-# platforms = ['Instagram', 'Threads', 'TikTok', 'YouTube']
 
 @app.route("/platforms", methods=("POST", "GET"))
 def social_platform_tbl():
-    # Get the names of the platforms from the database
+    """Get the platform names from the database"""
+
     platforms = ['Instagram', 'Threads', 'TikTok', 'YouTube']
     return render_template('table.html', platforms=platforms, title='Top 100 Influencers')
 
-# for platform in platforms:
-#     @app.route('/platforms/Top100/' + platform)
-#     def rank_table(platform):
-#         collection = db.all_platforms
-#         df = pd.DataFrame(list(collection.find()))
-#         df_platform = df.loc[df['Social Media Platform'] == platform]
-#         df_platform = df_platform.reset_index()
-#         df_platform = df_platform.rename(columns={"index":"Rank"})
-#         df_platform['Rank'] = df_platform.index + 1
-#         platform_df = df_platform[['Rank','NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
-
-#         return {'data': platform_df.to_dict('records')}
-
 @app.route('/platforms/Top100/Instagram')
 def insta_tbl():
-    collection = db.all_platforms
-    df = pd.DataFrame(list(collection.find()))
+    """Get Top 100 influencers on Instagram"""
+    
     df_platform = df.loc[df['Social Media Platform'] == 'Instagram']
     df_platform = df_platform.reset_index()
     df_platform = df_platform.rename(columns={"index":"Rank"})
     df_platform['Rank'] = df_platform.index + 1
-    # platform_df = df_platform[['NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
     platform_df = df_platform[['Rank','NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]  
     return {'data': platform_df.to_dict('records')}
 
 @app.route('/platforms/Top100/TikTok')
 def tiktok_tbl():
-    collection = db.all_platforms
-    df = pd.DataFrame(list(collection.find()))
+    """Get Top 100 influencers on TikTok"""
+
     df_platform = df.loc[df['Social Media Platform'] == 'TikTok']
     df_platform = df_platform.reset_index()
     df_platform = df_platform.rename(columns={"index":"Rank"})
     df_platform['Rank'] = df_platform.index + 1
-    platform_df = df_platform[['NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
+    platform_df = df_platform[['Rank','NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
   
     return {'data': platform_df.to_dict('records')}
 
 @app.route('/platforms/Top100/Threads')
 def threads_tbl():
-    collection = db.all_platforms
-    df = pd.DataFrame(list(collection.find()))
+    """Get Top 100 influencers on Threads"""
+
     df_platform = df.loc[df['Social Media Platform'] == 'Threads']
     df_platform = df_platform.reset_index()
     df_platform = df_platform.rename(columns={"index":"Rank"})
     df_platform['Rank'] = df_platform.index + 1
-    platform_df = df_platform[['NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
+    platform_df = df_platform[['Rank','NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
   
     return {'data': platform_df.to_dict('records')}
 
 @app.route('/platforms/Top100/YouTube')
 def youtube_tbl():
-    collection = db.all_platforms
-    df = pd.DataFrame(list(collection.find()))
+    """Get Top 100 influencers on YouTube"""
+
     df_platform = df.loc[df['Social Media Platform'] == 'YouTube']
     df_platform = df_platform.reset_index()
     df_platform = df_platform.rename(columns={"index":"Rank"})
     df_platform['Rank'] = df_platform.index + 1
-    platform_df = df_platform[['NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
+    platform_df = df_platform[['Rank','NAME','FOLLOWERS','COUNTRY','POTENTIAL REACH']]
   
     return {'data': platform_df.to_dict('records')}
 
-# horizontal bar plot of top x number of influencers by platform
 @app.route("/Top<topnumber>", methods=("POST", "GET"))
 def topnumber_influencers(topnumber):
 
     """ Show horizontal bar plot from all platforms by top x influencers """
-    # get the entire merged_df later
-    all_collection = db.all_platforms
-    all_df = pd.DataFrame(list(all_collection.find()))
 
     img = BytesIO()
 
     # Get top x influencers for each social media platform
-    top_x_instagram = all_df[all_df['Social Media Platform'] == 'Instagram'].nlargest(int(topnumber), 'FOLLOWERS')
-    top_x_youtube = all_df[all_df['Social Media Platform'] == 'YouTube'].nlargest(int(topnumber), 'FOLLOWERS')
-    top_x_tiktok = all_df[all_df['Social Media Platform'] == 'TikTok'].nlargest(int(topnumber), 'FOLLOWERS')
-    top_x_threads = all_df[all_df['Social Media Platform'] == 'Threads'].nlargest(int(topnumber), 'FOLLOWERS')
+    top_x_instagram = df[df['Social Media Platform'] == 'Instagram'].nlargest(int(topnumber), 'FOLLOWERS')
+    top_x_youtube = df[df['Social Media Platform'] == 'YouTube'].nlargest(int(topnumber), 'FOLLOWERS')
+    top_x_tiktok = df[df['Social Media Platform'] == 'TikTok'].nlargest(int(topnumber), 'FOLLOWERS')
+    top_x_threads = df[df['Social Media Platform'] == 'Threads'].nlargest(int(topnumber), 'FOLLOWERS')
 
     # Plotting top 10 influencers for each platform
     sns.set_theme(style="whitegrid")
@@ -189,7 +173,6 @@ def topnumber_influencers(topnumber):
     axs[1, 1].set_ylabel('Influencer')
 
     plt.tight_layout()
-
     plt.savefig(img, format='png')
     # https://stackoverflow.com/questions/65419083/simple-matplotlib-as-embedded-image-in-web-page-generated-by-flask
     plt.close()
@@ -199,48 +182,104 @@ def topnumber_influencers(topnumber):
 
     return render_template('hbarplot.html', plot_url=plot_url)
 
-# up to here - will not work until I pull in the merged df
-# https://stackoverflow.com/questions/61750811/dropdown-menu-for-plotly-choropleth-map-plots
-# interactive map - dropdown show different traces depending on ER, potential reach, followers, #influencers?
-# @app.route("/map", methods=("POST", "GET"))
-# def map():
-#     """Display interactive map?"""
-  
-#   return()
+@app.route('/influencersList')
+def influencer_list():
+    """ Get the names of the influencers from the database """
+    influencers = list(collection.distinct("NAME"))
+    return jsonify(influencers)
 
-# # list of countries
-# @app.route("/countries", methods=("POST", "GET"))
-# def countries():
-#     """List unique countries"""
-  
-#   return()
+@app.route('/influencers')
+def influencer_plot():
+    """ Get the names of the influencers from the database """
+    influencers = list(collection.distinct("NAME"))
+    return render_template('barplot.html', influencers=influencers)
 
-# # list of countries
-# @app.route("/<countries>", methods=("POST", "GET"))
-# def influencers_in_countries(countries):
-#   """Influencers from countries"""
-  
-#   return()
+@app.route('/followers_by_platform', methods=("POST", "GET"))
+def get_followers_by_platform():
+    """ Get data required for bar chart for selected influencer - not listed"""
 
-# # Influencer details - handle, platform
-# @app.route("/<name>", methods=("POST", "GET"))
-# def influencers_details(name):
-#   """Influencers details across social media platforms"""
-  
-#   return()
+    # Get the influencer's name from the request body
+    data = request.json
+    if 'name' not in data:
+        return jsonify({'error': 'Name of the influencer is required'}), 400
+    
+    influencer_name = data['name']
 
-# # Avg potential reach by social media platform and category
-# @app.route("/avg_reach", methods=("POST", "GET"))
-# def avg_reach():
-#   """Full graph - average potential reach by social media platform and category"""
-  
-#   return()
+    # Get the data of followers by social media platform for the specific influencer from the database
+    followers_by_platform = collection.aggregate([
+        {'$match': {'NAME': influencer_name}},
+        {'$group': {'_id': '$Social Media Platform', 'total_followers': {'$sum': '$FOLLOWERS'}}},
+        {'$sort': {'total_followers': -1}}
+    ])
+    data = {entry['_id']: entry['total_followers'] for entry in followers_by_platform}
+    return jsonify(data)
 
-# @app.route("/avg_reach/category", methods=("POST", "GET"))
-# def avg_reach():
-#   """Average potential reach by social media platform and category"""
-  
-#   return()
+@app.route('/PotentialReach_categories', methods=("POST", "GET"))
+def PotentialReach_categories():
+
+    img = BytesIO()
+
+    # Create the bar plot
+    plt.figure(figsize=(12, 6))
+    sns.barplot(x='Social Media Platform', y='POTENTIAL REACH', hue='CATEGORY', data=df)
+
+    # Set labels and title
+    plt.xlabel('Social Media Platform')
+    plt.ylabel('Potential Reach (millions)')
+
+    # Show the plot
+    plt.tight_layout()
+    plt.savefig(img, format='png')
+    plt.show()
+    plt.close()
+
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template('stackedbarplot.html', plot_url=plot_url, title='Potential reach by categories and platform')
+
+@app.route('/PotentialReach_box')
+def PotentialReach_box():
+    """Potential Reach box plot"""
+
+    img = BytesIO()
+
+    # Set the figure size
+    plt.figure(figsize=(10, 6))
+
+    # Create the box plot
+    sns.boxplot(x='Social Media Platform', y='POTENTIAL REACH', data=df)
+
+    # Set labels and title
+    plt.xlabel('Social Media Platform')
+    plt.ylabel('Potential Reach (millions)')
+
+    # Show the plot
+    plt.tight_layout()
+    plt.savefig(img, format='png')
+    plt.show()
+
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+    return render_template('stackedbarplot.html', plot_url=plot_url, title='Distribution of Potential Reach Across Social Media Platforms')
+
+@app.route("/map_followers", methods=("POST", "GET"))
+def map_followers():
+    """Interactive map - followers"""
+
+    return render_template('Map_based_on_followers_count_world_map.html', title='Map of influencers and their social media following')
+
+@app.route("/map_engagement", methods=("POST", "GET"))
+def map_engagement():
+    """Interactive map - engagement"""
+
+    return render_template('engagement_rate_world_map.html', title='Map of countries where Top 100 influencers are from and their engagement rate (%)')
+
+# list of countries
+@app.route("/countries", methods=("POST", "GET"))
+def countries():
+    """List unique countries"""
+    countries = list(collection.distinct("COUNTRY"))
+    return jsonify(countries)
 
 # Run the app
 if __name__ == '__main__': 
